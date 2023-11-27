6957404b4e56bdd2e81af9d931c78df02954e293
3041TAN-14 <3041TAN-14@boronkay.local>
2023-11-27 10:12:53 +0100

    frissitve

--- cardb.php
+++ cardb.php
@@ -4,9 +4,39 @@ ini_set('memory-limit', '-1');
 
 $fileName = 'car-db.csv';
 $csvData = getCsvData($fileName);
+if (empty($csvData)) {
+    echo "Nem található adat a csv fájlban.";
+    return false;
+}
+
+$header = $csvData[0];
+
+$keyMaker = array_search('make', $header);
+$keyModel = array_search('model', $header);
+$result = [];
+$maker = '';
+$model = '';
+$isHeader = true;
+
+for ($i = 1; count($csvData); $i++) {
+
+}
+foreach ($csvData as $data) {
+    if ($isHeader) {
+        $isHeader = false;
+        continue;
+    }
+    if ($maker != $data[$keyMaker]) {
+        $maker = $data[$keyMaker];
+    }
+    if ($model != $data[$keyModel]) {
+        $model = $data[$keyModel];
+        $result[$maker][] = $model;
+    }
+}
+print_r($result);
 
 $arr = array ('first' => 'a', 'second' => 'b',);
 $key = array_search ('a', $arr);
 
-$header = $csvData[0];
-$keyMaker = array_search('make', $csvData[0]);
+
